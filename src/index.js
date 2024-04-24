874bfbc5da051379ce103fcffccb33447fbdb740
leandro <leandro_antunes28@hotmail.com>
2024-04-24 01:16:34 -0300

    add package

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-const express = require("express");
-const rotas = require("./rotas");
-
 require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const rotas = require("./rotas");
 const { PORT } = process.env;
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.use(rotas);
 
 app.listen(PORT);
