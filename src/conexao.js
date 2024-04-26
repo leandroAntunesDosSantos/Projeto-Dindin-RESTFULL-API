a458234ab5746dd8e429a86c701a82f03d729e6d
leandro <leandro_antunes28@hotmail.com>
2024-04-26 15:35:47 -0300

    rel

--- src/conexao.js
+++ src/conexao.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const { DB_HOST, DB_PORT, DB_USER, DB_DATABASE, DB_PASSWORD, ENDPOINT_ID, SSL } = process.env;
+const { DB_HOST, DB_PORT, DB_USER, DB_DATABASE, DB_PASSWORD, ENDPOINT_ID, SSLMODE } = process.env;
 
 const { Pool } = require("pg");
 
@@ -9,7 +9,7 @@ const pool = new Pool({
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_DATABASE,
-  ssl:'true',
+  sslmode:SSLMODE,
   ssl: {
     rejectUnauthorized: false,
   },
