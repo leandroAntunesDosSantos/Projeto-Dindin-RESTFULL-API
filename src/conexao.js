1599cacbb91f878ea59214f50c575074234f8396
leandro <leandro_antunes28@hotmail.com>
2024-04-27 02:35:05 -0300

    comentado banco de dados

--- src/conexao.js
+++ src/conexao.js
@@ -1,16 +1,16 @@
-require("dotenv").config();
+// require("dotenv").config();
 
-const { Pool } = require("pg");
+// const { Pool } = require("pg");
 
-const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT,
-    ssl: {
-        rejectUnauthorized: true,
-    },
-});
+// const pool = new Pool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DATABASE,
+//     port: process.env.DB_PORT,
+//     ssl: {
+//         rejectUnauthorized: true,
+//     },
+// });
 
-module.exports = pool;
+// module.exports = pool;
