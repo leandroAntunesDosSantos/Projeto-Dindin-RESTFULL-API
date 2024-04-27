84820919806a81bd2a90a60ab78b556dfa2ac3dc
leandro <leandro_antunes28@hotmail.com>
2024-04-27 03:47:09 -0300

    requisicao categorias

--- src/controladores/categorias.js
+++ src/controladores/categorias.js
@@ -2,10 +2,8 @@ const knex = require("../conexao");
 
 const listarCategorias = async (req, res) => {
   try {
-    const categoria = req.categoria;
-    const listaCategoria = await knex("categorias").select("*");
-
-    return res.status(200).json(listaCategoria.rows);
+    const listaCategoria = await knex("categorias")
+    return res.status(200).json(listaCategoria);
   } catch (erro) {
     return res.status(500).json({ mensagem: "ocorreu um erro" });
   }
