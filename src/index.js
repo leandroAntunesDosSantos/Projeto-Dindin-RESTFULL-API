bc333ab2a0e3f99e9c95255c21a6a50b2c86274d
leandro <leandro_antunes28@hotmail.com>
2024-04-27 17:09:00 -0300

    verificando cors

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
+//const cors = require("cors");
 
 const rotas = require("./rotas");
 const { PORT } = process.env;
 
 const app = express();
 
-app.use(cors());
+//app.use(cors());
 app.use(express.json());
 
 app.use(rotas);
